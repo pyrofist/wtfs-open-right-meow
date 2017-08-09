c73831edbab01e621582db0925f8809bde84f9c1
Jeremy <jeremy.h.lee7@gmail.com>
2017-08-09 14:36:07 -0700

    Basic validation tests for User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe User do
+  context 'validations' do
+    it { is_expected.to validate_presence_of :username }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :password }
+    subject { FactoryGirl.build(:user) }
+    it { is_expected.to validate_uniqueness_of :username }
+    it { is_expected.to validate_uniqueness_of :email }
+  end
+end
