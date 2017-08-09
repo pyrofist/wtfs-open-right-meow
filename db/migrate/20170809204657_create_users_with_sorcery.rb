b42a8ea31191f889ef6098f5ca02f0081d551720
Jeremy <jeremy.h.lee7@gmail.com>
2017-08-09 14:03:46 -0700

    Add username field to user

--- db/migrate/20170809204657_create_users_with_sorcery.rb
+++ db/migrate/20170809204657_create_users_with_sorcery.rb
@@ -1,11 +1,12 @@
 class CreateUsersWithSorcery < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :email,            :null => false
+      t.string :email,            null: false
+      t.string :username,         null: false
       t.string :crypted_password
       t.string :salt
 
-      t.timestamps                :null => false
+      t.timestamps                null: false
     end
 
     add_index :users, :email, unique: true
