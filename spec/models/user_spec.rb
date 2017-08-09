ad0fbecce6d81cddf391e41042f1832474f30636
Jeremy <jeremy.h.lee7@gmail.com>
2017-08-09 14:47:08 -0700

    Minor cleanup, fix tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,6 @@ describe User do
   context 'validations' do
     it { is_expected.to validate_presence_of :username }
     it { is_expected.to validate_presence_of :email }
-    it { is_expected.to validate_presence_of :password }
     subject { FactoryGirl.build(:user) }
     it { is_expected.to validate_uniqueness_of :username }
     it { is_expected.to validate_uniqueness_of :email }
