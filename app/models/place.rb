cb61a95cfa0cb7a99c9673dccbb2a0ab4cceecba
Jeremy <jeremy.h.lee7@gmail.com>
2017-08-09 15:35:11 -0700

    Models and helpers

---
+++ app/models/place.rb
@@ -0,0 +1,20 @@
+class Place
+  def initialize(latitude, longitude)
+    @latitude = latitude
+    @longitude = longitude
+    @url_string = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{latitude},#{longitude}&rankby=distance&opennow=true&type=restaurant&key=#{ENV["GOOGLEPLACE"]}"
+  end
+
+  def results_lookup
+    @response_hash = APIHelper.get_response_as_hash(@url_string)
+
+    if @response_hash["status"] == "OK"
+      results = @response_hash["results"].map do |result|
+        {name: result["name"], address: result["vicinity"]}
+      end
+      results
+    else
+      []
+    end
+  end
+end
