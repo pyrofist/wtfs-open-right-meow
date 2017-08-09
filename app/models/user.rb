e65890cfc7c27951e9c848400dbb87b4ab9b9d3a
Jeremy <jeremy.h.lee7@gmail.com>
2017-08-09 14:30:10 -0700

    Add not-nil validation on email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true
-  validates :username, uniqueness: true
+  validates_uniqueness_of :email, :username
+  validates_presence_of :email, :username
 end
