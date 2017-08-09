5e9cc10384b01720337375ad2c93eebc25d04ae1
Jeremy <jeremy.h.lee7@gmail.com>
2017-08-09 13:52:52 -0700

    Rename sorcery generated migration to be more clear

--- db/migrate/20170809204657_create_users_with_sorcery.rb
+++ db/migrate/20170809204657_create_users_with_sorcery.rb
@@ -1,4 +1,4 @@
-class SorceryCore < ActiveRecord::Migration[5.1]
+class CreateUsersWithSorcery < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :email,            :null => false
