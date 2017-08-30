6aa4bc2e2e089ac765fbe339041b1aeae11f15c0
Jeremy <jeremy.h.lee7@gmail.com>
2017-08-30 14:18:27 -0700

    Provide example object for uniqueness validation

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -4,6 +4,7 @@ describe Location do
   context 'validations' do
     it { is_expected.to validate_presence_of :latitude }
     it { is_expected.to validate_presence_of :longitude }
+    subject { FactoryGirl.build(:location)}
     it { is_expected.to validate_uniqueness_of(:latitude).scoped_to :longitude }
   end
 end
